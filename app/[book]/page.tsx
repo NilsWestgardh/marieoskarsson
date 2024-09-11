51b16d7eac2b44ab8f93b00147817562dac62199
Nils Westgårdh <72079716+NilsWestgardh@users.noreply.github.com>
2024-09-11 22:09:23 +0200

    commit

--- app/[book]/page.tsx
+++ app/[book]/page.tsx
@@ -11,17 +11,64 @@ import {
 } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
+// Icons
+import { MdOpenInNew } from "react-icons/md";
 
-export default function Book() {
+export default async function Book({ 
+  params 
+}: { 
+  params: { project: string } 
+}) {
+  // Fetch books
+  // If no books, return 404
+  const placeholderBook = {
+    title: "Book",
+    image: "/images/book.jpg",
+    description: "Book",
+  }
+  
   return (
-    <Card>
-      <CardHeader>
-        <CardTitle>Book</CardTitle>
-        <CardDescription>Book</CardDescription>
-      </CardHeader>
-      <CardContent>
-        <CardDescription>Book</CardDescription>
-      </CardContent>
-    </Card>
+    <div
+      id="book-container"
+      className="
+        flex
+        flex-col
+        justify-start
+        items-start
+        w-full
+        gap-4
+      "
+    >
+      <div
+        className="
+          grid
+          grid-cols-1
+          md:grid-cols-2
+          w-full
+        "
+      >
+
+      </div>
+      <Image
+        src={placeholderBook.image}
+        alt={placeholderBook.title}
+        width={500}
+        height={500}
+        className="
+          w-full
+          h-auto
+          rounded-lg
+        "
+      />
+      <Card>
+        <CardHeader>
+          <CardTitle>{placeholderBook.title}</CardTitle>
+          <CardDescription>{placeholderBook.description}</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <CardDescription>Book</CardDescription>
+        </CardContent>
+      </Card>
+    </div>
   );
 }
