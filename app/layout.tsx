996f51d48a15dde96d75593dfdea539335de569e
Nils Westgårdh <72079716+NilsWestgardh@users.noreply.github.com>
2024-09-11 21:41:51 +0200

    Initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,71 +1,82 @@
-import DeployButton from "@/components/deploy-button";
-import { EnvVarWarning } from "@/components/env-var-warning";
-import HeaderAuth from "@/components/header-auth";
-import { ThemeSwitcher } from "@/components/theme-switcher";
-import { hasEnvVars } from "@/utils/supabase/check-env-vars";
-import { GeistSans } from "geist/font/sans";
-import { ThemeProvider } from "next-themes";
-import Link from "next/link";
-import "./globals.css";
+// Utils
+import dynamic from 'next/dynamic';
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from '@vercel/speed-insights/next';
+// Styles
+import { Inter } from "next/font/google";
+import "@/app/lib/styles/globals.css";
+// Metadata
+import type { Metadata } from "next";
+import { metadataKeywords } from "@/app/lib/data/data"
 
-const defaultUrl = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
-  : "http://localhost:3000";
+const PostHogPageView = dynamic(() => import('./PostHogPageView'), { ssr: false })
+const inter = Inter({ subsets: ["latin"] });
+const defaultUrl = process.env.VERCEL_PROJECT_PRODUCTION_URL
+  ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
+  : process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000";
 
-export const metadata = {
+export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  keywords: metadataKeywords,
+  title: {
+    default: "Nils Westgardh Portfolio",
+    template: `%s | Nils Westgardh`,
+  },
+  openGraph: {
+    description: "Nils Westgardh is an award-winning advertising creative director skilled in brand stategy, art direction, copywriting, design, and web development.",
+    images: [
+      `${defaultUrl}/opengraph-image.png`,
+      `${defaultUrl}/twitter-image.png`,
+    ],
+  }
 };
 
-export default function RootLayout({
-  children,
-}: {
+type RootLayoutProps = {
   children: React.ReactNode;
-}) {
-  return (
-    <html lang="en" className={GeistSans.className} suppressHydrationWarning>
-      <body className="bg-background text-foreground">
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <main className="min-h-screen flex flex-col items-center">
-            <div className="flex-1 w-full flex flex-col gap-20 items-center">
-              <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-                <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
-                  <div className="flex gap-5 items-center font-semibold">
-                    <Link href={"/"}>Next.js Supabase Starter</Link>
-                    <div className="flex items-center gap-2">
-                      <DeployButton />
-                    </div>
-                  </div>
-                  {!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
-                </div>
-              </nav>
-              <div className="flex flex-col gap-20 max-w-5xl p-5">
-                {children}
-              </div>
+};
 
-              <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
-                <p>
-                  Powered by{" "}
-                  <a
-                    href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-                    target="_blank"
-                    className="font-bold hover:underline"
-                    rel="noreferrer"
-                  >
-                    Supabase
-                  </a>
-                </p>
-                <ThemeSwitcher />
-              </footer>
-            </div>
-          </main>
-        </ThemeProvider>
+export default function RootLayout(
+  props: RootLayoutProps
+) {
+  const { children } = props;
+
+  return (
+    <html 
+      lang="en" 
+      className={inter.className}
+    >
+      <body
+        className="
+          bg-background 
+          text-foreground
+        "
+      >
+        <PHProvider>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="light"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <PostHogPageView />
+            <Navigation />
+            <main
+              className="
+                min-h-screen 
+                flex 
+                flex-col 
+                items-center 
+                gap-4
+                w-full
+                mt-20
+              "
+            >
+              {children}
+            </main>
+            <SpeedInsights />
+            <Analytics />
+          </ThemeProvider>
+        </PHProvider>
       </body>
     </html>
   );
